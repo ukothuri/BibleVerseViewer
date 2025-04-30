be169f3d08d05d0ca34d2615e3e7044d22aa3c6e
Mahesh Kothuri <101145397+ukothuri@users.noreply.github.com>
2025-04-30 00:20:00 -0700

    Add unit test case.

---
+++ app/src/test/java/com/cambly/bibleverseviewer/utils/MainDispatcherRule.kt
@@ -0,0 +1,21 @@
+package com.cambly.bibleverseviewer.utils
+
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    private val dispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
