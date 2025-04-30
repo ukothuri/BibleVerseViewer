9fc11290dd98f502610305637cca417b5fb28466
Mahesh Kothuri <101145397+ukothuri@users.noreply.github.com>
2025-04-29 23:53:18 -0700

    Add UI components with MVVM.

--- app/src/main/java/com/cambly/bibleverseviewer/data/remote/BibleApiService.kt
+++ app/src/main/java/com/cambly/bibleverseviewer/data/remote/BibleApiService.kt
@@ -1,8 +1,8 @@
 package com.cambly.bibleverseviewer.data.remote
 
 
-import retrofit2.http.GET
 import retrofit2.Response
+import retrofit2.http.GET
 
 interface BibleApiService {
     @GET("matt%2025:31-33,46")
--- app/src/main/java/com/cambly/bibleverseviewer/ui/screen/BibleVerseList.kt
+++ app/src/main/java/com/cambly/bibleverseviewer/ui/screen/BibleVerseList.kt
@@ -13,15 +13,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.cambly.bibleverseviewer.data.model.Verse
 import com.cambly.bibleverseviewer.viewmodel.BibleViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 
 @Composable
